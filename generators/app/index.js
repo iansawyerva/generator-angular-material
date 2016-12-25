85aea253cf0e48938f2a83dea53cbd92a9215b8f
iansawyerva <iansawyerva@gmail.com>
2016-12-24 23:04:26 -0500

    Generator simple prompt created

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,33 +4,33 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = Generator.extend({
-  prompting: function () {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the extraordinary ' + chalk.red('generator-angular-material') + ' generator!'
-    ));
+    prompting: function() {
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to ' + chalk.red('Angular Material')
+        ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+        return this.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'What would you like to name your app? Default: ' + this.appname,
+            default: this.appname
+        }]).then((answers) => {
+            if (this.appname != answers.name) {
+                this.appname = answers.name
+            }
+            this.log('Selected app name: ', answers.name);
+        });
+    },
 
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
-  },
+    writing: function() {
+        this.fs.copy(
+            this.templatePath('./'),
+            this.destinationPath(this.appname)
+        );
+    },
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  },
-
-  install: function () {
-    this.installDependencies();
-  }
+    install: function() {
+        this.log(this.appname + ' has been created!');
+    }
 });
