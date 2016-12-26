f08dc6d83c55650fac7aaf2f4d4d7d877bb3b248
Ian Sawyer <iansawyerva@gmail.com>
2016-12-26 02:27:06 -0500

    Adding full template examples

--- generators/component/index.js
+++ generators/component/index.js
@@ -7,7 +7,22 @@ var util = require('util');
 var service = yeoman.extend({
 
     writing: function(arg1, arg2) {
-        this.component = this.fs.read(this.templatePath(arg1 + '.html'));
+        this.appname = process.cwd().match(/([^\/]*)\/*$/)[1];
+        this.controllerName = arg1.replace(/ /g, ' ');
+        this.fs.copyTpl(
+            this.templatePath('./' + arg1 + '.js'),
+            this.destinationPath('./public/js/controllers/components/' + this.controllerName + '-controller.js'), {
+                AppName: this.appname,
+                ControllerName: this.controllerName + 'Controller'
+            }
+        ),
+        this.fs.copyTpl(
+            this.templatePath('./' + arg1 + '.html'),
+            this.destinationPath(this.templatePath('./tmp/' + arg1 + '.html')), {
+                ControllerName: this.controllerName + 'Controller'
+            }
+        );
+        this.component = this.fs.read(this.templatePath('./tmp/' + arg1 + '.html'));
         this.partial = this.fs.read(arg2);
         this.partial = this.partial.replace('{{component}}', this.component)
         this.fs.write(arg2, this.partial);
